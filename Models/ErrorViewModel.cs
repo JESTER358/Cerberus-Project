a97bde0615b6818a1ee6b90cc6fc7c15be089388
JESTER358 <omar.gaona@uabc.edu.mx>
2026-03-23 17:43:58 -0700

    Semana 1: Estructura y NuGet instalado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoInnovador.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
